b310e30c72c7de1ecdc193b559251ad78f3b6007
xinbai-ee <u1299165@gmail.com>
2026-02-25 18:13:26 -0700

    image update

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -60,7 +60,7 @@ export const experienceEvents = [
     bgImage:
       "https://images.unsplash.com/photo-1532094349884-543bc11b234d?auto=format&fit=crop&w=1200&q=80",
     contentImage:
-      "https://imgur.com/a/njsKtk1",
+      "https://imgur.com/eUx2q0I",
     showContentImageInCard: true, // 控制图片是否在卡片中展示
   },
   {
